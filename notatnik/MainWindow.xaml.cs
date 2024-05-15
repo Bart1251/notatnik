0f0a33f11529e6d5090a912f105609307d284b5f
Bartłomiej Więcek <bartlomiej.wiecek@vp.pl>
2024-05-15 11:11:46 +0200

    added saveing and opening methods

--- notatnik/MainWindow.xaml.cs
+++ notatnik/MainWindow.xaml.cs
@@ -54,6 +54,126 @@ namespace notatnik
             text.Select(selectionStart, 0);
         }
 
-        
+        private void OpenEv(object sender, RoutedEventArgs e)
+        {
+            Open();
+        }
+
+        private void Open()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            ofd.Multiselect = false;
+            ofd.Title = "Otwórz";
+            if (ofd.ShowDialog() == true)
+            {
+                text.Text = File.ReadAllText(ofd.FileName);
+                path = ofd.FileName;
+                saved = true;
+            }
+        }
+
+        private void SaveAsEv(object sender, RoutedEventArgs e)
+        {
+            SaveAs();
+        }
+
+        private bool SaveAs()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt";
+            sfd.AddExtension = true;
+            sfd.Title = "Zapisz jako";
+            if(sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, text.Text);
+                path = sfd.FileName;
+                saved = true;
+                return true;
+            }
+            return false;
+        }
+
+        private void SaveEv(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            if(path == "")
+            {
+                SaveAs();
+            }
+            else
+            {
+                File.WriteAllText(path, text.Text);
+                saved = true;
+            }
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (path != "")
+                if(File.ReadAllText(path) != text.Text)
+                    saved = false;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if(!saved)
+            {
+                MessageBoxResult result = MessageBox.Show("Czy chcesz zapisać zmiany w pliku?", "Notatnik", MessageBoxButton.YesNoCancel);
+                if(result == MessageBoxResult.Yes)
+                {
+                    if(path == "")
+                    {
+                        if(!SaveAs())
+                            e.Cancel = true;
+                    }
+                    else
+                    {
+                        Save();
+                    }
+                }
+                else if(result == MessageBoxResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+            base.OnClosing(e);
+        }
+
+        private void ShortcutCheck(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                if (e.Key == Key.O)
+                {
+                    Open();
+                    Keyboard.ClearFocus();
+                }
+                if (e.Key == Key.S)
+                {
+                    Save();
+                    Keyboard.ClearFocus();
+                }
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                    if (e.Key == Key.S)
+                    {
+                        SaveAs();
+                        Keyboard.ClearFocus();
+                    }
+
+            }
+        }
     }
 }
